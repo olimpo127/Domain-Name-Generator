9034c485982700e4c34b1e830f60bfca5869451c
F.Ojeda.I <fojedai47@gmail.com>
2023-01-07 00:12:46 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let pronoun = ["the", "our"];
+let noun = ["jogger", "racoon"];
+let com = ".com";
+let adj = ["great", "big"];
+console.log(pronoun[0] + adj[0] + noun[0] + com);
+
+console.log("OJEDA TEST")
--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let com = ".com";
+  //console.log(pronoun[0] + adj[0] + noun[0] + com);
+  //console.log(pronoun[0] + adj[0] + noun[1] + com);
+  //console.log(pronoun[0] + adj[1] + noun[1] + com);
+  //console.log(pronoun[0] + adj[1] + noun[0] + com);
+  //console.log(pronoun[1] + adj[0] + noun[0] + com);
+  //console.log(pronoun[1] + adj[0] + noun[1] + com);
+  //console.log(pronoun[1] + adj[1] + noun[0] + com);
+  //console.log(pronoun[1] + adj[1] + noun[1] + com);
+  for (let i = 0; i < 2; i++) {
+    console.log(pronoun[0] + adj[0] + noun[i] + com);
+    console.log(pronoun[0] + adj[1] + noun[i] + com);
+    console.log(pronoun[1] + adj[0] + noun[i] + com);
+    console.log(pronoun[1] + adj[1] + noun[i] + com);
+  }
 };
